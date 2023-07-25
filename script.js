6faeed4d88093e490e96d064a5c1021002c64499
Milena <milena0202@icloud.com>
2023-07-25 17:44:20 +0300

    New function

--- script.js
+++ script.js
@@ -1,38 +1,104 @@
+const form = document.querySelector(".form");
 const input = document.querySelector(".input");
-const ul = document.querySelector(".list__conteiner");
 const button = document.querySelector(".button");
-button.addEventListener("click", () => {
-  if (input.value === "") {
+const ul = document.querySelector(".ul");
+const delCompleted = document.querySelector(".delete__completed");
+const delAll = document.querySelector(".delete__all");
+const buttons = document.querySelector(".buttons");
+const hr = document.querySelector("hr");
+// LocalStorage
+const KEY = "todos";
+const saveToDo = JSON.parse(localStorage.getItem(KEY)) ?? [];
+let todos = saveToDo;
+// Функция по отображению кнопок и линии
+const showButtons = () => {
+  if (!todos.length) {
+    buttons.style.display = "none";
+    hr.style.display = "none";
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = 
-    `<input type="checkbox" class="task__input">
-     <span class="task__text">${input.value}</span>
-     <button class="task__button">❌</button>`;
-    ul.appendChild(li);
+    buttons.style.display = "flex";
+    hr.style.display = "block";
   }
-  input.value = "";
-  saveData();
-});
-ul.addEventListener(
-  "click",
-  (el) => {
-    if (el.target.tagName === "LI") {
-      el.target.classList.toggle("checked");
-      saveData();
-    } else if (el.target.tagName === "BUTTON") {
-      el.target.parentElement.remove();
-      saveData();
-    }
-  },
-  false
-);
-function saveData() {
-  localStorage.setItem("data", ul.innerHTML);
-}
-function showTask() {
-  ul.innerHTML = localStorage.getItem("data");
-}
-showTask();
+};
+// Функция по созданию задач
+const drawTodo = (obj) => {
+  const li = document.createElement("li");
+  li.innerHTML = `<label><input type="checkbox" class="task__input">
+                  <span class="task__text">${obj.text}</span>;
+                  <button class="task__button">❌</button></label>`;
+  const chk = li.querySelector(".task__input");
+  const del = li.querySelector(".task__button");
+  chk.checked = obj.completed;
+  del.addEventListener("click", () => deleteTask(obj.id));
+  chk.addEventListener("change", () => toggleComplete(obj.id));
+  return li;
+};
+// Поиск по ID Checkbox
+const toggleComplete = (id) => {
+  const findedTodo = todos.find((todo) => todo.id === id);
+  findedTodo.completed = !findedTodo.completed;
+  renderTodos();
+};
+// Функция по отображению задач
+const renderTodos = () => {
+  ul.innerHTML = "";
+  todos.forEach((el) => {
+    ul.append(drawTodo(el));
+  });
+  localStorage.setItem(KEY, JSON.stringify(todos));
+  showButtons();
+};
+// Функция по добавлению задач
+const addTask = (evt) => {
+  evt.preventDefault();
+  const value = input.value;
+  if (value.trim().length) {
+    todos.push({
+      id: Date.now(),
+      text: value,
+      completed: false,
+    });
+    input.value = "";
+    input.focus();
+    renderTodos();
+  }
+};
+//Функция по удалению задач
+const deleteTask = (id) => {
+  todos = todos.filter((todo) => todo.id !== id);
+  renderTodos();
+};
+// Функция по удалению всех задач
+const deleteAll = () => {
+  todos = [];
+  renderTodos();
+};
+// События
+form.addEventListener("submit", addTask);
+delAll.addEventListener("click", deleteAll);
+renderTodos();
 
+// Функция по удалению выбранных задач
+function box() {
+  let boxes = document.getElementsByClassName("task__input");
+  let labels = document.getElementsByTagName("label");
+  for (let i = 0; i < boxes.length; i++) {
+    if (boxes[i].checked) {
+      labels[i].classList.add("active");
+      labels[i].classList.remove("posive");
+    } else {
+      labels[i].classList.add("posive");
+      labels[i].classList.remove("active");
+    }
+  }
+}
+box();
+delCompleted.addEventListener("click", func);
+function func() {
+  let types = document.getElementsByClassName("active");
+  while (types[0]) {
+    types[0].remove();
+  }
+  renderTodos()
+}
 
